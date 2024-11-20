730cf3f41aad43f13e94b7fde16ec1acdadfeeb5
0xponpa <cristian97rojas@gmail.com>
2024-11-20 23:38:10 +0100

    Update Modo monitor wlan0.sh

--- Modo monitor/Modo monitor wlan0.sh
+++ Modo monitor/Modo monitor wlan0.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Obtener fecha y hora actual en formato día-mes-año
+# obtenemos fecha y hora en formato d-m-Y
 fecha=$(date +"%d-%m-%Y_%H-%M-%S")
 
-# Poner tarjeta en modo monitor
+# se pone tarjeta en modo monitor
 sudo airmon-ng start wlan0
 sleep 5  # Esperar 5 segundos
 
-# Empezar monitorización
-#sudo airodump-ng --band a wlan0mon
+# se empieza monitorizacion
 sudo airodump-ng wlan0mon --band abg
-sleep 5  # Esperar 5 segundos
+
+sleep 5  # esperar 5 segundos
